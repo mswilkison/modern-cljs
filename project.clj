e52aa4ea7787767bc196812330caa2d7bc6eec3c
MacLane Wilkison <mwilkison@gmail.com>
2014-05-16 11:33:55 -0700

    update source-paths and add/configure lein-cljsbuild plugin

--- project.clj
+++ project.clj
@@ -1,6 +1,27 @@
 (defproject modern-cljs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "ClojureScript tutorial app"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+
+  ; clj and cljs source path code
+  :source-paths ["src/cj" "src/cljs"]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  
+  ; lein-cljsbuild plugin to build a cljs project
+  :plugins [[lein-cljsbuild "1.0.0"]]
+
+  ; cljsbuild options configuation
+  :cljsbuild {:builds
+              [{; cljs source code path
+                :source-paths ["src/cljs"]
+                
+                ; Google Closure (CLS) options configuration
+                :compiler {; CLS generated JS script filename
+                           :output-to "resources/public/js/modern.js"
+
+                           ; minimal JS optimization directive
+                           :optimizations :whitespace
+
+                           ; generated JS code prettyfication
+                           :pretty-print true}}]})
