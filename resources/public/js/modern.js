d36c32020249b0601a6b31369de21cbcd7b266e2
MacLane Wilkison <mwilkison@gmail.com>
2014-05-16 12:46:05 -0700

    client-side login validation

--- resources/public/js/modern.js
+++ resources/public/js/modern.js
@@ -30742,3 +30742,31 @@ clojure.browser.repl.connect.call(null, "http://localhost:9000/repl");
 goog.provide("modern_cljs.modern");
 goog.require("cljs.core");
 document.write("Hello, ClojureScript!");
+goog.provide("modern_cljs.login");
+goog.require("cljs.core");
+modern_cljs.login.validate_form = function validate_form() {
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+  if(cljs.core.count.call(null, email.value) > 0 && cljs.core.count.call(null, password.value) > 0) {
+    return true
+  }else {
+    alert("Please, complete the form!");
+    return false
+  }
+};
+modern_cljs.login.init = function init() {
+  if(cljs.core.truth_(function() {
+    var and__3273__auto__ = document;
+    if(cljs.core.truth_(and__3273__auto__)) {
+      return document.getElementById
+    }else {
+      return and__3273__auto__
+    }
+  }())) {
+    var login_form = document.getElementById("loginForm");
+    return login_form.onsubmit = modern_cljs.login.validate_form
+  }else {
+    return null
+  }
+};
+window.onload = modern_cljs.login.init;
