5117e38c4eda6bb7888d7307035d9389b0af3bec
MacLane Wilkison <mwilkison@gmail.com>
2014-05-16 14:11:24 -0700

    add pre-prod and prod builds

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   ; clj and cljs source path code
-  :source-paths ["src/clj" "src/cljs"]
+  :source-paths ["src/clj" "src/cljs" "src/brepl"]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2069"]
                  [compojure "1.1.6"]
@@ -17,17 +17,42 @@
 
   ; cljsbuild options configuation
   :cljsbuild {:builds
-              [{; cljs source code path
-                :source-paths ["src/cljs"]
-                
+              {:dev
+               {; cljs source code path
+                :source-paths ["src/brepl" "src/cljs"]
+
                 ; Google Closure (CLS) options configuration
                 :compiler {; CLS generated JS script filename
-                           :output-to "resources/public/js/modern.js"
+                           :output-to "resources/public/js/modern_dbg.js"
 
                            ; minimal JS optimization directive
                            :optimizations :whitespace
 
                            ; generated JS code prettyfication
-                           :pretty-print true}}]}
+                           :pretty-print true}}
+               :prod
+               {; cljs source code path
+                :source-paths ["src/brepl" "src/cljs"]
+
+                :compiler {; the name of emitted JS script file
+                           :output-to "resources/public/js/modern.js"
+
+                           ; advanced optimization
+                           :optimizations :advanced
+
+                           ; no prettyfication
+                           :pretty-print false}}
+               :pre-prod
+               {; cljs source code path
+                :source-paths ["src/cljs"]
+
+                :compiler {; different output name
+                           :output-to "resources/public/js/modern_pre.js"
+
+                           ; simple optimization
+                           :optimizations :simple
+
+                           ;no prettyfication
+                           :pretty-print false}}}}
   
   :ring {:handler modern-cljs.core/handler})
