4fb7433bd3ce3daa922b8ca7986724a18013694b
MacLane Wilkison <mwilkison@gmail.com>
2014-05-16 12:11:16 -0700

    add compojure dependency and add routing handler to core.clj

--- project.clj
+++ project.clj
@@ -5,12 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   ; clj and cljs source path code
-  :source-paths ["src/cj" "src/cljs"]
+  :source-paths ["src/clj" "src/cljs"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2069"]]
+                 [org.clojure/clojurescript "0.0-2069"]
+                 [compojure "1.1.6"]]
   
   ; lein-cljsbuild plugin to build a cljs project
-  :plugins [[lein-cljsbuild "1.0.0"]]
+  :plugins [[lein-cljsbuild "1.0.0"]
+            [lein-ring "0.8.8"]]
 
   ; cljsbuild options configuation
   :cljsbuild {:builds
@@ -25,4 +27,6 @@
                            :optimizations :whitespace
 
                            ; generated JS code prettyfication
-                           :pretty-print true}}]})
+                           :pretty-print true}}]}
+  
+  :ring {:handler modern-cljs.core/handler})
