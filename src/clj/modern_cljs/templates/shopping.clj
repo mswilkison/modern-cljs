2d134285d4cf643c9134b3128ceb3fa369ba4c2d
MacLane Wilkison <mwilkison@gmail.com>
2014-05-17 22:47:54 -0700

    inject form validators into Enlive shopping template

--- src/clj/modern_cljs/templates/shopping.clj
+++ src/clj/modern_cljs/templates/shopping.clj
@@ -1,12 +1,30 @@
 (ns modern-cljs.templates.shopping
-  (:require [net.cgrand.enlive-html :refer [deftemplate set-attr]]
-            [modern-cljs.remotes :refer [calculate]]))
+  (:require [net.cgrand.enlive-html :refer [deftemplate content do-> add-class set-attr attr=]]
+            [modern-cljs.remotes :refer [calculate]]
+            [modern-cljs.shopping.validators :refer [validate-shopping-form]]))
 
-(deftemplate shopping "public/shopping.html"
-  [quantity price tax discount]
-  [:#quantity] (set-attr :value quantity)
-  [:#price] (set-attr :value price)
-  [:#tax] (set-attr :value tax)
-  [:#discount] (set-attr :value discount)
-  [:#total] (set-attr :value
-                      (format "%.2f" (double (calculate quantity price tax discount)))))
+(defmacro maybe-error [expr]
+  `(if-let [x# ~expr]
+     (do-> (add-class "error")
+           (content x#))
+     identity))
+
+(deftemplate update-shopping-form "public/shopping.html"
+  [q p t d e]
+
+  [[:label (attr= :for "quantity")]] (maybe-error (first (:quantity e)))
+  [[:label (attr= :for "price")]] (maybe-error (first (:price e)))
+  [[:label (attr= :for "tax")]] (maybe-error (first (:tax e)))
+  [[:label (attr= :for "discount")]] (maybe-error (first (:discount e)))
+  
+  [:#quantity] (set-attr :value q)
+  [:#price] (set-attr :value p)
+  [:#tax] (set-attr :value t)
+  [:#discount] (set-attr :value d)
+  [:#total] (if e
+              (set-attr :value "0.00")
+              (set-attr :value
+                         (format "%.2f" (double (calculate q p t d))))))
+
+(defn shopping [q p t d]
+  (update-shopping-form q p t d (validate-shopping-form q p t d)))
